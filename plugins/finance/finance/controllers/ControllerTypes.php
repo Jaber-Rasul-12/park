ba5c2657b72a8ee0d1f8d8a674c955e8b0e0add2
jaber <jaber@acornassoicated.org>
2026-08-08 13:51:20 +0300

    udpate

--- plugins/finance/finance/controllers/ControllerTypes.php
+++ plugins/finance/finance/controllers/ControllerTypes.php
@@ -19,4 +19,21 @@ class ControllerTypes extends Controller
         parent::__construct();
         BackendMenu::setContext('Finance.Finance', 'finance_menu', 'types');
     }
+
+
+               public function formGetRedirectUrl($context = null, $model = null)
+    {
+               $url = post('url');
+        if (($url == 'create') && !empty($url)) {
+            return "finance/finance/controllertypes/create";
+        }else if (($url == 'preview') && !empty($url)) {
+            return "finance/finance/controllertypes/$url/$model->id";
+        }else {
+            if ((post("close") == 1) && !empty(post("close"))) {
+                return "finance/finance/controllertypes";
+            } else {
+                return "finance/finance/controllertypes/update/$model->id";
+            }
+        }
+    }
 }
